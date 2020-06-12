2e223cb082fd7fa540538995b50e2acf784ccbfc
Jagadeesh <jagadeesh@rebaca.com>
2020-06-12 21:02:49 +0530

    updated port logic to work with heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+let PORT = process.env.PORT || 3000;
+
 app.get("/alive", (req, res) => {
   res.send(`I'm Alive.`);
 });
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000...`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
